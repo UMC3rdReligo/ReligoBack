9681870d197c7bff2f5dd70310ed08601e2b2b6a
MyunghyunNero <mh1kim0910@gmail.com>
2023-01-19 23:16:10 +0900

    [feat] : UserDetails 생성

---
+++ src/main/java/com/umcreligo/umcback/global/config/security/userdetails/PrincipalUserDetails.java
@@ -0,0 +1,54 @@
+package com.umcreligo.umcback.global.config.security.userdetails;
+
+import com.umcreligo.umcback.domain.user.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class PrincipalUserDetails  implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(RoleType.USER.getKey()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
