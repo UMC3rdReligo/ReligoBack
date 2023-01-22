6e1739d05353219a59fbc27f3aafb0921b8a34a8
Na Jongwoo <njw1204@naver.com>
2023-01-22 23:33:53 +0900

    [feat] Church - 교단 엔티티 추가

---
+++ src/main/java/com/umcreligo/umcback/domain/church/repository/PlatformRepository.java
@@ -0,0 +1,7 @@
+package com.umcreligo.umcback.domain.church.repository;
+
+import com.umcreligo.umcback.domain.church.domain.Platform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformRepository extends JpaRepository<Platform, Long> {
+}
