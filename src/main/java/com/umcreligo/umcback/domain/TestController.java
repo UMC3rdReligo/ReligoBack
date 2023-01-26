05bda59b636e35fa19c32af4143d2198435251b8
MyunghyunNero <mh1kim0910@gmail.com>
2023-01-27 01:05:35 +0900

    [fix] testController Request 수정

--- src/main/java/com/umcreligo/umcback/domain/TestController.java
+++ src/main/java/com/umcreligo/umcback/domain/TestController.java
@@ -3,7 +3,7 @@ package com.umcreligo.umcback.domain;
 import com.umcreligo.umcback.global.config.service.S3Service;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +16,7 @@ public class TestController {
     private S3Service s3Service;
 
     @PostMapping("/gallery")
-    public String execWrite(@RequestParam("images") MultipartFile file ) throws IOException {
+    public String execWrite(@RequestPart("file") MultipartFile file ) throws IOException {
         String imgPath = s3Service.upload(file);
 
 
